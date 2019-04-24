e9fb9d3222fb0171e0fda125b07f4c380376f73a
ertugrul.sen <ertugrul.sen@accenture.com>
2019-04-24 10:04:52 +0300

    first commit

---
+++ src/main/java/com/boot/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.boot.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "First Spring Boot Application";
+
+    }
+}
